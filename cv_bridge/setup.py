e05be301083853a8a0a6733cea0e7a828ea8a6fb
shschaefer <stuart@theschaefers.com>
2018-08-01 07:38:30 -0700

    Windows bringup.

--- cv_bridge/setup.py
+++ cv_bridge/setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+import sys
 
 d = generate_distutils_setup()
 
 d['packages'] = ['cv_bridge']
-d['package_dir'] = {'' : 'python/'}
+if sys.platform != 'win32':
+  d['package_dir'] = {'' : 'python/'}
+else:
+  d['package_dir'] = {'' : 'python'}
 
 setup(**d)
